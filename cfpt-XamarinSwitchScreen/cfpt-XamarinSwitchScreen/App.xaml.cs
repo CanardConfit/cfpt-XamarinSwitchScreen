1d935d62d4f97040c90d3ee85f79333f2de936da
CanardConfit <canardconfit.developpement@gmail.com>
2020-11-26 19:41:53 +0100

    Update

---
+++ cfpt-XamarinSwitchScreen/cfpt-XamarinSwitchScreen/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace cfpt_XamarinSwitchScreen
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+        
+        /// <summary>
+        /// Fonction qui permet de changer la page actuellement affichée.
+        /// </summary>
+        /// <param name="page">Page à affiché</param>
+        public void ChangeScreen(Page page)
+        {
+            MainPage = page;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ cfpt-XamarinSwitchScreen/cfpt-XamarinSwitchScreen/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace cfpt_XamarinSwitchScreen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Evenement du bouton.
+        /// </summary>
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            // Permet de changer de page
+            ((App)App.Current).ChangeScreen(new AnotherPage());
+        }
+    }
+}
